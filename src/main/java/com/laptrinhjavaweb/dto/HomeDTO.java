3d5705d38fee53e9a6f4da4c7b005358fa4f72b1
vietnh27320 <vietnhhe140392@fpt.edu.vn>
2020-12-17 21:05:42 +0700

    spring-mvc

---
+++ src/main/java/com/laptrinhjavaweb/dto/HomeDTO.java
@@ -0,0 +1,15 @@
+package com.laptrinhjavaweb.dto;
+
+public class HomeDTO {
+	private String anhYeuem;
+
+	public String getAnhYeuem() {
+		return anhYeuem;
+	}
+
+	public void setAnhYeuem(String anhYeuem) {
+		this.anhYeuem = anhYeuem;
+	}
+	
+
+}
